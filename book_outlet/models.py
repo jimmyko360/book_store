ce8aed33c16c555c3b612e6070e1f1510e375cf3
Jimmy Ko <jimmy.ko@nyu.edu>
2023-07-03 19:47:10 -0400

    instantiated first model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.Charfield(max_length=255)
+    rating = models.IntegerField()
+    # id = models.AutoField()
+    # ^ Django will already add this for you
