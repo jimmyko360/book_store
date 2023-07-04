9bda4bc73383de7a9a82774698965d61fafb2c08
Jimmy Ko <jimmy.ko@nyu.edu>
2023-07-04 03:32:09 -0400

    add author and is_bestselling to Book model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     # id = models.AutoField()
     # ^ Django will already add this for you
+    author = models.CharField(null=True, max_length=255)
+    is_bestselling = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
