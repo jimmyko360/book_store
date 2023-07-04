d847fb6f56ccc312daf3195a26b2f1f8e76ada0a
Jimmy Ko <jimmy.ko@nyu.edu>
2023-07-03 20:03:21 -0400

    fix typo

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.Charfield(max_length=255)
+    title = models.CharField(max_length=255)
     rating = models.IntegerField()
     # id = models.AutoField()
     # ^ Django will already add this for you
