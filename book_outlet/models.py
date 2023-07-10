515fce8c16da8c46082642206b591499589ad11e
Jimmy Ko <jimmy.ko@nyu.edu>
2023-07-10 00:05:50 -0400

    override get_absolute_url method on Book model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -8,6 +9,10 @@ class Book(models.Model):
     # ^ Django will already add this for you
     author = models.CharField(null=True, max_length=255)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book_by_id", args=[self.id])
+    # ^this is a native model method that we override
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
