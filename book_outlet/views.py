16932c2ee450a0a5a1172e07b0d38dfa80b134e1
Jimmy Ko <jimmy.ko@nyu.edu>
2023-07-09 23:07:32 -0400

    added individual book url, view, and template

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Book
+# ^using just "from models" doesn't work for some reason
 
 def index(request):
-    return render(request, "book_outlet/index.html")
+    try:
+      book_list = Book.objects.all()
+      return render(request, "book_outlet/index.html", {"book_list": book_list})
+    except:
+       raise Http404()
+
+def book_by_id(request, id):
+    # try:
+    #   book = Book.objects.get(pk=id)
+    #   # ^pk means primary key. you can still use id=id but this is more
+    #   return render(request, "book_outlet/book_detail.html", {"book": book})
+    # except:
+    #   raise Http404()
+
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html", {"book": book})
