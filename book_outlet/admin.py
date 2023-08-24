2df71b5fdc98a5022df20368648af38abb77269f
Jimmy Ko <jimmy.ko@nyu.edu>
2023-08-24 18:44:56 -0400

    add list_filter and list_display to BookAdmin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -6,5 +6,7 @@ class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
     # a property cannot be both in readonly_fields and in prepopulated_fields
+    list_filter = ("author", "rating", "is_bestselling",)
+    list_display = ("title", "author", "is_bestselling")
 
 admin.site.register(Book, BookAdmin)
