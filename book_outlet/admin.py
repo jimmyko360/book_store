e057d7ceb5ed80c953c04647b06f9a9e28e840fd
Jimmy Ko <jimmy.ko@nyu.edu>
2023-08-24 16:39:35 -0400

    changed slug into a prepopulated_field
    removed custom save method override on Book model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -3,6 +3,8 @@ from .models import Book
 
 
 class BookAdmin(admin.ModelAdmin):
-    readonly_fields = ("slug",)
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    # a property cannot be both in readonly_fields and in prepopulated_fields
 
 admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,15 +10,23 @@ class Book(models.Model):
     # ^ Django will already add this for you
     author = models.CharField(null=True, max_length=255)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(
+        default="",
+        # we no longer need these properties if we use either readonly_fields or prepopulated_fields in admin.py
+        # blank=True,
+        # editable=False,
+        null=False,
+        db_index=True
+    )
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
     # ^this is a native model method that we override
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
+    # we moved this work to be performed by BookAdmin
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
