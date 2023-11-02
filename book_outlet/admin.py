aed8698404c80866616e916df715d2b7bbc71380
Jimmy Ko <ko.jimmy09@gmail.com>
2023-11-02 12:48:54 -0400

    add Author model to admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
     # a property cannot be both in readonly_fields and in prepopulated_fields
-    list_filter = ("author", "rating", "is_bestselling",)
+    list_filter = (
+        "author",
+        "rating",
+        "is_bestselling",
+    )
     list_display = ("title", "author", "is_bestselling")
 
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
